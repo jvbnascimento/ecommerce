a8e653fabb1a7fb88a811d76dc334a72fb75bd53
XitharisHidan <joao.vitoor.batista@gmail.com>
2020-02-11 13:58:53 -0300

    atualizando templates, instalando bibliotecas para conexão com banco de dados

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -5,10 +5,16 @@ from app import app
 from app.models.login import LoginForm
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    user = ''#{'username': 'João Vitor'}
+    user = {'username': 'João Vitor'}
     form = LoginForm(request.form)
+
+    if form.validate_on_submit():
+        flash('Login solicitado por email {}, lembrar_me={}'.format(
+            form.email.data, form.lembrar_me.data))
+        print("here")
+        return redirect('/index')
     
     return render_template('index.html', title='Ecommerce - Página Inicial', user=user, form=form)
 
@@ -21,4 +27,4 @@ def login():
             form.email.data, form.lembrar_me.data))
         return redirect('/index')
 
-    return render_template('modelo_login.html', title='Sign In', form = form)
+    return render_template('login.html', title='Ecommerce - Página Inicial', form = form)
