aebdcc09fd238e32b00f652ea927042bcc3509bb
XitharisHidan <joao.vitoor.batista@gmail.com>
2020-02-11 16:18:29 -0300

    atualizando requirements.txt

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -7,13 +7,12 @@ from app.models.login import LoginForm
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    user = {'username': 'João Vitor'}
+    user = ''#{'username': 'João Vitor'}
     form = LoginForm(request.form)
 
     if form.validate_on_submit():
         flash('Login solicitado por email {}, lembrar_me={}'.format(
             form.email.data, form.lembrar_me.data))
-        print("here")
         return redirect('/index')
     
     return render_template('index.html', title='Ecommerce - Página Inicial', user=user, form=form)
