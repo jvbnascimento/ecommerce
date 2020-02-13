e4fb144c6ccaf98ab41ca4ae8c59abd8051907a3
XitharisHidan <joao.vitoor.batista@gmail.com>
2020-02-13 10:28:17 -0300

    melhorando controle de login

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -10,7 +10,7 @@ from app.models.usuario import Usuario
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('index_user'))
     
     form = LoginForm(request.form)
     usuario = ''
@@ -23,14 +23,21 @@ def index():
             return redirect(url_for('login'))
         
         login_user(usuario, remember = form.lembrar_me.data)
-        # return redirect(url_for('index'))
-    
+        return redirect(url_for('index_user'))
+
     return render_template('index.html', title='Ecommerce - Página Inicial', user=usuario, form=form)
+
+@app.route('/user')
+def index_user():
+    if current_user.is_authenticated:
+        return render_template('index.html', title='Ecommerce - Página Inicial - ' + current_user.nome, user=current_user)
+    return redirect(url_for('index'))
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('index_user'))
     
     form = LoginForm(request.form)
 
@@ -42,7 +49,7 @@ def login():
             return redirect(url_for('login'))
         
         login_user(usuario, remember = form.lembrar_me.data)
-        # return redirect(url_for('index'))
+        return redirect(url_for('index_user'))
 
     return render_template('login.html', title='Ecommerce - Página Inicial', form = form)
 
